92e083824b874eaf5c38a068d469f9dd7c1e508d
DakshinGhai <dakshinghai14@gmail.com>
2026-01-26 14:47:41 +0530

    uploading and fetching product image

--- SpringEcom/src/main/java/com/dakshin/SpringEcom/model/Product.java
+++ SpringEcom/src/main/java/com/dakshin/SpringEcom/model/Product.java
@@ -1,10 +1,8 @@
 package com.dakshin.SpringEcom.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,8 +26,13 @@ public class Product{
     private String brand;
     private BigDecimal price;
     private String category;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date releaseDate;
     private boolean productAvailable;
     private int stockQuantity;
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
 
 }
--- SpringEcom/src/main/java/com/dakshin/SpringEcom/service/ProductService.java
+++ SpringEcom/src/main/java/com/dakshin/SpringEcom/service/ProductService.java
@@ -5,7 +5,9 @@ import com.dakshin.SpringEcom.model.Product;
 import com.dakshin.SpringEcom.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -16,4 +18,16 @@ public class ProductService {
     public List<Product> getAllProducts(){
         return productRepo.findAll();
     };
+
+    public Product getProductById(int id) {
+        return productRepo.findById(id).orElse(null);
+    }
+
+
+    public Product addProduct(Product product, MultipartFile image) throws IOException {
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+        return productRepo.save(product);
+    }
 }
